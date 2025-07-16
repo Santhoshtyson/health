993d14ccc30cc88e67ecd00bfd6697d64a06bf66
Santhosh Kumar T <santhoshkumar.t1@SH-LAP-15074.local>
2025-07-16 12:42:40 +0530

    second commmit

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+
 import React, { useState, useMemo } from 'react';
 import { Header } from './components/Header';
 import { ComparisonTable } from './components/ComparisonTable';
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig, loadEnv } from 'vite';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: '/health/', 
       server: {
         host: '0.0.0.0',
         port: 5173
